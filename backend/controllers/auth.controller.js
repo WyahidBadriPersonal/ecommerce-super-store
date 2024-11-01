15c22226e83448cb6c1004513d3e463e9875cb91
WyahidBadriPersonal <NotWyahid@gmail.com>
2024-11-01 13:09:35 -0400

    schema added and sign up added

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,20 @@
+import User from "../models/user.model.js"
+
 export const signup = async (req, res) => {
-    res.send("Sign up route called")
+    const { name, email, password } = req.body
+    try {
+        const userExist = await User.findOne({ email })
+
+        if (userExist) {
+            return res.status(400).json({ message: "User already exists" })
+        }
+    
+        const user = await User.create({ name, email, password })
+    
+        res.status(201).json({ user })
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
 }
 
 export const login = async (req, res) => {
