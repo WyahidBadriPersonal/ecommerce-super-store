59087625f1272326ccd8bae29525737750426302
WyahidBadriPersonal <NotWyahid@gmail.com>
2024-11-01 16:54:25 -0400

    added redis

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,35 @@
 import User from "../models/user.model.js"
+import jwt from "jsonwebtoken"
+import {redis} from "../lib/redis.js"
+const generateToken = (userId) => {
+    const accessToken = jwt.sign({ userId }, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "15m",
+    })
+    const refreshToken = jwt.sign({ userId }, process.env.REFRESH_TOKEN_SECRET, {
+        expiresIn: "7d",
+    })
+
+    return { accessToken, refreshToken }
+}
+
+const storeRefreshToken = async (userId, refreshToken) => {
+    await redis.set(`refresh_token:${userId}`, refreshToken, "EX", 60 * 60 * 24 * 7)
+}
+
+const setCookies = (res, accessToken, refreshToken) => {
+    res.cookie("accessToken", accessToken, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict",
+        maxAge: 15 * 60 * 1000,
+    })
+    res.cookie("refreshToken", refreshToken, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict",
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+    })
+}
 
 export const signup = async (req, res) => {
     const { name, email, password } = req.body
@@ -10,8 +41,19 @@ export const signup = async (req, res) => {
         }
     
         const user = await User.create({ name, email, password })
-    
-        res.status(201).json({ user })
+
+        //Authentication
+        const {accessToken, refreshToken} = generateToken(user._id)
+        await storeRefreshToken(user._id, refreshToken)
+        setCookies(res, accessToken, refreshToken)
+
+        res.status(201).json({ user:{_id: 
+            user._id, 
+            name: user.name,
+            email: user.email,
+            role: user.role,
+        }, message: "User created successfully"}) 
+        
     } catch (error) {
         res.status(500).json({ message: error.message })
     }
